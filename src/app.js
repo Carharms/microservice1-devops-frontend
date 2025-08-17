306581e4ff0702ff0fb02a427774d1de515f724f
Carter Harms <harmsc@uchicago.edu>
2025-08-17 17:40:36 -0500

    Update V0.7.1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ function App() {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
+        // alteration for push
         // Fetch products
         axios.get('http://localhost:3001/products')
             .then(res => setProducts(res.data))
