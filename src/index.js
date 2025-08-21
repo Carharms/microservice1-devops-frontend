9ea64559ee5944c537354720151073ec50ec7a4e
Carter Harms <harmsc@uchicago.edu>
2025-08-20 20:36:00 -0500

    Updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 
 // alteration for push
+// alteration for push
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
